037785e09e6c26337b307b75bdeafd6c47fe67de
Sebastián Alcántar Pérez <112568851+Sebas8920@users.noreply.github.com>
2024-03-15 16:38:12 +0000

    04

---
+++ miPrincipal/DemoDelimitadores.java
@@ -0,0 +1,32 @@
+package miPrincipal;
+
+public class DemoDelimitadores {
+    public static void menu(){
+        System.out.println("************************");
+        System.out.println("      DELIMITADORES     ");
+        System.out.println("************************");
+        System.out.println();
+        Delimitadores objDel = new Delimitadores();
+        String expr = "(a+b)/2";
+        if (objDel.evaluacion(expr))
+            System.out.println("Expresión correcta");
+        else
+            System.out.println("Expresión Incorrecta");
+        expr= "while (m<(n[8]+o)) { "+
+                     "int p=7; "+
+                     "/*comentarios*/"+
+                     "               } ";
+        if (objDel.evaluacion(expr))
+           System.out.println("Expresión correcta");
+        else
+           System.out.println("Expresión Incorrecta");
+        expr= "while (m<(n[8]+o)) { "+
+              "int p=7; "+
+              "/*comentarios*/";
+        if (objDel.evaluacion(expr))
+            System.out.println("Expresión correcta");
+        else
+            System.out.println("Expresión Incorrecta");
+    }
+    
+}
